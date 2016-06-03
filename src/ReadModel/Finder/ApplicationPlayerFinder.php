54c93a9577695e310ce2a03206dd95333d109c41
remi-san <remi.san@gmail.com>
2016-06-03 19:13:28 +0000

    added methods to user player finder

--- src/ReadModel/Finder/ApplicationPlayerFinder.php
+++ src/ReadModel/Finder/ApplicationPlayerFinder.php
@@ -3,6 +3,7 @@
 namespace MiniGameMessageApp\ReadModel\Finder;
 
 use MessageApp\User\ApplicationUserId;
+use MiniGame\Entity\MiniGameId;
 use MiniGame\Entity\PlayerId;
 use MiniGameApp\ReadModel\Player;
 
@@ -23,6 +24,26 @@ interface ApplicationPlayerFinder
      * @return Player The player.
      */
     public function getActivePlayerForUser(ApplicationUserId $userId);
+
+    /**
+     * Gets the active player for the user and game type
+     *
+     * @param  ApplicationUserId $userId
+     * @param  string            $gameType
+     *
+     * @return Player The player.
+     */
+    public function getActivePlayerForUserAndGameType(ApplicationUserId $userId, $gameType);
+
+    /**
+     * Gets the active player for the user and game type
+     *
+     * @param  ApplicationUserId $userId
+     * @param  MiniGameId        $gameId
+     *
+     * @return Player The player.
+     */
+    public function getActivePlayerForUserAndGame(ApplicationUserId $userId, MiniGameId $gameId);
 
     /**
      * @param  Player $player
